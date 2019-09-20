13c6afd2dba4e61b83c7d96bf9663d634058e36e
lisa <lisa@odinsoftware.co.kr>
2019-09-20 17:18:39 +0900

    ..

---
+++ src/exchanges/upbit/private/ordersChance.cs
@@ -0,0 +1,164 @@
+﻿namespace CCXT.Collector.Upbit.Private
+{
+    /// <summary>
+    /// 매수/매도 시 제약사항
+    /// </summary>
+    public class OrderConstraint
+    {
+        /// <summary>
+        /// 화폐를 의미하는 영문 대문자 코드
+        /// </summary>
+        public string currency
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 주문금액 단위
+        /// </summary>
+        public decimal price_unit
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 최소 매도/매수 금액
+        /// </summary>
+        public decimal min_total
+        {
+            get;
+            set;
+        }
+    }
+
+    /// <summary>
+    /// 마켓에 대한 정보
+    /// </summary>
+    public class OrderMarketInfo
+    {
+        /// <summary>
+        /// 마켓의 유일 키
+        /// </summary>
+        public string id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 마켓 이름
+        /// </summary>
+        public string name
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 지원 주문 방식
+        /// </summary>
+        public string[] order_types
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 지원 주문 종류
+        /// </summary>
+        public string[] order_sides
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 매수 시 제약사항
+        /// </summary>
+        public OrderConstraint bid
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 매도 시 제약사항
+        /// </summary>
+        public OrderConstraint ask
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 최대 매도/매수 금액
+        /// </summary>
+        public decimal max_total
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 마켓 운영 상태
+        /// </summary>
+        public string state
+        {
+            get;
+            set;
+        }
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class OrdersChance
+    {
+        /// <summary>
+        /// 매수 수수료 비율
+        /// </summary>
+        public decimal bid_fee
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 매도 수수료 비율
+        /// </summary>
+        public decimal ask_fee
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 마켓에 대한 정보
+        /// </summary>
+        public OrderMarketInfo market
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 매수 시 사용하는 화폐의 계좌 상태
+        /// </summary>
+        public Accounts bid_account
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 매도 시 사용하는 화폐의 계좌 상태
+        /// </summary>
+        public Accounts ask_account
+        {
+            get;
+            set;
+        }
+   }
+}
--- src/exchanges/upbit/private/privateApi.cs
+++ src/exchanges/upbit/private/privateApi.cs
@@ -1,5 +1,6 @@
 ﻿using CCXT.NET.Coin;
 using CCXT.NET.Coin.Private;
+using CCXT.NET.Coin.Types;
 using CCXT.NET.Upbit;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -38,15 +39,174 @@ namespace CCXT.Collector.Upbit.Private
         /// 전체 계좌 조회: 내가 보유한 자산 리스트를 보여줍니다.
         /// </summary>
         /// <returns></returns>
-        public async Task<List<Accounts>> Accounts()
+        public async Task<ApiResult<List<Accounts>>> GetAccounts()
         {
-            var _result = new List<Accounts>();
+            var _result = new ApiResult<List<Accounts>>();
 
-            var _json_value = await privateClient.CallApiGet1Async("/accounts");
+            var _response = await privateClient.CallApiGet2Async("/accounts");
+            if (_response.IsSuccessful == true)
+            {
+                _result.result = privateClient.DeserializeObject<List<Accounts>>(_response.Content);
+                _result.SetSuccess();
+            }
 
-            var _json_result = privateClient.GetResponseMessage(_json_value.Response);
-            if (_json_result.success == true)
-                _result = privateClient.DeserializeObject<List<Accounts>>(_json_value.Content);
+            return _result;
+        }
+
+        /// <summary>
+        /// 주문 가능 정보: 마켓별 주문 가능 정보를 확인한다.
+        /// </summary>
+        /// <param name="baseId">코인명</param>
+        /// <param name="quoteId">화폐명</param>
+        /// <returns></returns>
+        public async Task<ApiResult<OrdersChance>> GetOrdersChance(string baseId, string quoteId)
+        {
+            var _result = new ApiResult<OrdersChance>();
+
+            var _params = new Dictionary<string, object>();
+            {
+                _params.Add("market", $"{quoteId}-{baseId}");
+            }
+
+            var _response = await privateClient.CallApiGet2Async("/orders/chance", _params);
+            if (_response.IsSuccessful == true)
+            {
+                _result.result = privateClient.DeserializeObject<OrdersChance>(_response.Content);
+                _result.SetSuccess();
+            }
+
+            return _result;
+        }
+
+        /// <summary>
+        /// 개별 주문 조회: 주문 UUID 를 통해 개별 주문건을 조회한다.
+        /// </summary>
+        /// <param name="orderId">주문 UUID</param>
+        /// <returns></returns>
+        public async Task<ApiResult<Order>> GetOrder(string orderId)
+        {
+            var _result = new ApiResult<Order>();
+
+            var _params = new Dictionary<string, object>();
+            {
+                _params.Add("uuid", $"{orderId}");
+            }
+
+            var _response = await privateClient.CallApiGet2Async("/order", _params);
+            if (_response.IsSuccessful == true)
+            {
+                _result.result = privateClient.DeserializeObject<Order>(_response.Content);
+                _result.SetSuccess();
+            }
+
+            return _result;
+        }
+
+        /// <summary>
+        /// 주문 리스트 조회: 주문 리스트를 조회한다.
+        /// </summary>
+        /// <param name="baseId">코인명</param>
+        /// <param name="quoteId">화폐명</param>
+        /// <returns></returns>
+        public async Task<ApiResult<List<Order>>> GetOrders(string baseId, string quoteId)
+        {
+            var _result = new ApiResult<List<Order>>();
+
+            var _params = new Dictionary<string, object>();
+            {
+                _params.Add("market", $"{quoteId}-{baseId}");
+                _params.Add("state", "wait");
+                _params.Add("page", 1);
+                _params.Add("order_by", "asc");
+            }
+
+            var _response = await privateClient.CallApiGet2Async("/orders", _params);
+            if (_response.IsSuccessful == true)
+            {
+                _result.result = privateClient.DeserializeObject<List<Order>>(_response.Content);
+                _result.SetSuccess();
+            }
+
+            return _result;
+        }
+
+        /// <summary>
+        /// 전체 주문 리스트 조회: 전체 주문 리스트를 조회한다.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ApiResult<List<Order>>> GetAllOrders()
+        {
+            var _result = new ApiResult<List<Order>>();
+
+            var _params = new Dictionary<string, object>();
+            {
+                _params.Add("state", "wait");
+                _params.Add("page", 1);
+                _params.Add("order_by", "asc");
+            }
+
+            var _response = await privateClient.CallApiGet2Async("/orders", _params);
+            if (_response.IsSuccessful == true)
+            {
+                _result.result = privateClient.DeserializeObject<List<Order>>(_response.Content);
+                _result.SetSuccess();
+            }
+
+            return _result;
+        }
+
+        /// <summary>
+        /// 주문 취소 접수: 주문 UUID를 통해 해당 주문에 대한 취소 접수를 한다.
+        /// </summary>
+        /// <param name="orderId">주문 UUID</param>
+        /// <returns></returns>
+        public async Task<ApiResult<Order>> DeleteOrder(string orderId)
+        {
+            var _result = new ApiResult<Order>();
+
+            var _params = new Dictionary<string, object>();
+            {
+                _params.Add("uuid", $"{orderId}");
+            }
+
+            var _response = await privateClient.CallApiDelete2Async("/order", _params);
+            if (_response.IsSuccessful == true)
+            {                
+                _result.result = privateClient.DeserializeObject<Order>(_response.Content);
+                _result.SetSuccess();
+            }
+
+            return _result;
+        }
+
+        /// <summary>
+        /// 주문하기: 주문 요청을 한다.
+        /// </summary>
+        /// <param name="baseId">코인명</param>
+        /// <param name="quoteId">화폐명</param>
+        /// <param name="quantity">주문 수량</param>
+        /// <param name="price">유닛당 주문 가격</param>
+        /// <param name="sideType">주문 타입</param>
+        /// <returns></returns>
+        public async Task<ApiResult<Order>> PutOrder(string baseId, string quoteId, decimal quantity, decimal price, SideType sideType)
+        {
+            var _result = new ApiResult<Order>();
+
+            var _params = new Dictionary<string, object>();
+            {
+                _params.Add("market", $"{quoteId}-{baseId}");
+                _params.Add("side", sideType == SideType.Bid ? "bid" : "ask");
+                _params.Add("volume", quantity);
+                _params.Add("price", price);
+                _params.Add("ord_type", "limit");
+            }
+
+            var _response = await privateClient.CallApiPut2Async("/orders", _params);
+            if (_response.IsSuccessful == true)
+            {
+                _result.result = privateClient.DeserializeObject<Order>(_response.Content);
+                _result.SetSuccess();
+            }
 
             return _result;
         }
